85350f47a73b515b9634a8ea9f04fe41bd8f1765
Piotr Wlodek <piotr.wlodek@gmail.com>
2010-09-11 15:11:46 +0200

    Touch samples updated

--- TouchSamples/TouchSamples.Sample1/MainWindow.xaml.cs
+++ TouchSamples/TouchSamples.Sample1/MainWindow.xaml.cs
@@ -20,6 +20,13 @@ namespace TouchSamples.Sample1
             InitializeComponent();
         }
 
+        /// <summary>
+        /// Create shape of given size, random fill and translate it to X,Y.
+        /// </summary>
+        /// <param name="x">X.</param>
+        /// <param name="y">Y.</param>
+        /// <param name="size">Desired size.</param>
+        /// <returns>New shape.</returns>
         private Shape GetShape(double  x, double y, Size size)
         {
             var elipse = new Ellipse();
@@ -37,12 +44,21 @@ namespace TouchSamples.Sample1
 
         private void OnTouchDown(object sender, TouchEventArgs e)
         {
+            // Get the touch point
             var touchPoint = e.GetTouchPoint(m_Canvas);
-            var size = (touchPoint.Size.IsEmpty || touchPoint.Size.Height == 0.0 || touchPoint.Size.Width == 0.0)
-                           ? new Size(100, 100)
-                           : touchPoint.Size;
-            var shape = GetShape(touchPoint.Position.X, touchPoint.Position.Y, size);
+
+            // Get the touch size
+            var touchSize = touchPoint.Size;
             
+            // If the touch device does not return proper size of the touch, create the shape with a default size
+            var shapeSize = (touchSize.IsEmpty || touchSize.Height == 0.0 || touchSize.Width == 0.0)
+                           ? new Size(100, 100)
+                           : touchSize;
+
+            // Create the shape
+            var shape = GetShape(touchPoint.Position.X, touchPoint.Position.Y, shapeSize);
+            
+            // Stash the shape and add it to the canvas
             m_Shapes.Add(touchPoint.TouchDevice.Id, shape);
             m_Canvas.Children.Add(shape);
 
@@ -51,9 +67,13 @@ namespace TouchSamples.Sample1
 
         private void OnTouchUp(object sender, TouchEventArgs e)
         {
+            // Get the touch point
             var touchPoint = e.GetTouchPoint(m_Canvas);
-            var shape = m_Shapes[touchPoint.TouchDevice.Id];
 
+            // Retrieve the shape associated with this touch
+            var shape = m_Shapes[touchPoint.TouchDevice.Id];
+            
+            // Release touch capture and remove the shape
             m_Canvas.ReleaseTouchCapture(touchPoint.TouchDevice);
             m_Canvas.Children.Remove(shape);
             m_Shapes.Remove(touchPoint.TouchDevice.Id);
@@ -63,10 +83,14 @@ namespace TouchSamples.Sample1
 
         private void OnTouchMove(object sender, TouchEventArgs e)
         {
+            // Get the touch point
             var touchPoint = e.GetTouchPoint(m_Canvas);
+
+            // Retrieve the shape associated with this touch
             var shape = m_Shapes[touchPoint.TouchDevice.Id];
             var translateTransform = (TranslateTransform) shape.RenderTransform;
 
+            // Update shape's position
             translateTransform.X = touchPoint.Position.X - shape.Width / 2;
             translateTransform.Y = touchPoint.Position.Y - shape.Height / 2;
 
--- TouchSamples/TouchSamples.Sample2/MainWindow.xaml.cs
+++ TouchSamples/TouchSamples.Sample2/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Windows;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Shapes;
 
 namespace TouchSamples.Sample2
 {
@@ -10,10 +11,82 @@ namespace TouchSamples.Sample2
     /// </summary>
     public partial class MainWindow
     {
+        private readonly Random m_Rnd = new Random((int)DateTime.Now.Ticks);
+
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        private void OnWindowLoaded(object sender, RoutedEventArgs e)
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                var shape = GenerateRandomShape();
+                
+                // Add shape to the canvas
+                m_Canvas.Children.Add(shape);
+            }
+        }
+
+        #region Shape Generation
+
+        /// <summary>
+        /// Generate random ellipse or rectangle.
+        /// </summary>
+        /// <returns>Random shape.</returns>
+        private Shape GenerateRandomShape()
+        {
+            const double minSize = 50;
+            const double maxSize = 250;
+
+            bool regular = m_Rnd.Next(0, 2) == 0;
+            bool isRectangle = m_Rnd.Next(0, 2) == 0;
+            double width, height;
+
+            if (regular)
+            {
+                width = height = m_Rnd.NextDouble() * maxSize + minSize;
+            }
+            else
+            {
+                width = m_Rnd.NextDouble() * maxSize + minSize;
+                height = m_Rnd.NextDouble() * maxSize + minSize;
+            }
+
+            var size = new Size(width, height);
+            var shape = isRectangle ? new Rectangle() : (Shape) new Ellipse();
+
+            SetupShape(shape, size,
+                       Math.Max(m_Rnd.NextDouble() * ActualWidth - size.Width / 2, size.Width / 2),
+                       Math.Max(m_Rnd.NextDouble() * ActualHeight - size.Height / 2, size.Height / 2));
+
+            return shape;
+        }
+
+        /// <summary>
+        /// Give a shape proper size, random fill, translate it to X,Y and enable manipulation.
+        /// </summary>
+        /// <param name="shape">Shape to be set up.</param>
+        /// <param name="size">Desired size.</param>
+        /// <param name="x">X.</param>
+        /// <param name="y">Y.</param>
+        private void SetupShape(Shape shape, Size size, double x, double y)
+        {
+            var matrix = new Matrix();
+            matrix.OffsetX = x - size.Width / 2;
+            matrix.OffsetY = y - size.Height / 2;
+
+            shape.Fill = new SolidColorBrush(Color.FromRgb((byte)m_Rnd.Next(256), (byte)m_Rnd.Next(256), (byte)m_Rnd.Next(256)));
+            shape.StrokeThickness = 2;
+            shape.Stroke = Brushes.Black;
+            shape.Width = size.Width;
+            shape.Height = size.Height;
+            shape.RenderTransform = new MatrixTransform(matrix);
+            shape.IsManipulationEnabled = true; // this enables manipulation on this element
+        }
+
+        #endregion
 
         private void OnManipulationStarting(object sender, ManipulationStartingEventArgs e)
         {
@@ -23,38 +96,37 @@ namespace TouchSamples.Sample2
 
         private void OnManipulationDelta(object sender, ManipulationDeltaEventArgs e)
         {
-            // Get the UI element and its RenderTransform matrix.
+            // Get the UI element and its RenderTransform matrix
             var uiElement = e.OriginalSource as UIElement;
             if (uiElement != null)
             {
                 var matrixTransform = (MatrixTransform) uiElement.RenderTransform;
                 var matrix = matrixTransform.Matrix;
 
-                // Rotate the Rectangle.
+                // Rotate the element
                 matrix.RotateAt(e.DeltaManipulation.Rotation,
                                 e.ManipulationOrigin.X,
                                 e.ManipulationOrigin.Y);
 
-                // Resize the Rectangle.  Keep it square 
-                // so use only the X value of Scale.
+                // Resize the element
                 matrix.ScaleAt(e.DeltaManipulation.Scale.X,
-                               e.DeltaManipulation.Scale.X,
+                               e.DeltaManipulation.Scale.Y,
                                e.ManipulationOrigin.X,
                                e.ManipulationOrigin.Y);
 
-                // Move the Rectangle.
+                // Move the element
                 matrix.Translate(e.DeltaManipulation.Translation.X,
                                  e.DeltaManipulation.Translation.Y);
 
-                // Apply the changes to the UI element.
+                // Apply the changes to the element
                 matrixTransform.Matrix = matrix;
 
                 var manipulationContainer = (FrameworkElement) e.ManipulationContainer;
                 var containingRect = new Rect(manipulationContainer.RenderSize);
                 var shapeBounds = uiElement.RenderTransform.TransformBounds(new Rect(uiElement.RenderSize));
 
-                // Check if the rectangle is completely in the window.
-                // If it is not and intertia is occuring, stop the manipulation.
+                // Check if the element is completely in the window,
+                // if it is not and intertia is occuring, stop the manipulation
                 if (e.IsInertial && !containingRect.Contains(shapeBounds))
                 {
                     e.Complete();
