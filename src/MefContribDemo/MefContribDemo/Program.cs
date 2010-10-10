26e6ecec50db72906e4b6bae54cb2ce44ee3645b
Piotr Wlodek <piotr.wlodek@gmail.com>
2010-10-10 18:33:29 +0200

    Added FactoryExportProvider demo

--- src/MefContribDemo/MefContribDemo/Program.cs
+++ src/MefContribDemo/MefContribDemo/Program.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace MefContribDemo
 {
@@ -9,7 +6,7 @@ namespace MefContribDemo
     {
         public static void Main(string[] args)
         {
-            
+            new FactoryDemo().Run();
         }
     }
 }
